42024ba045abe7ab1fff0c81b2dc62240db699dd
Jacob Beal <jacob.beal@ieee.org>
2019-05-19 01:22:29 +1200

    Update spell file and font

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #execute symlinks
-find . -maxdepth 1 -type d -not -path '*/\.*' | grep -v '^\.$' | xargs stow
+find . -maxdepth 1 -type d -not -path '*/\.*' -not -name 'installers' | grep -v '^\.$' | xargs stow
 
 
 #Tmux Plugin Manager
@@ -10,3 +10,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 #Vim Plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl https://git.io/fisher --create-dirs -sLo \
+    ~/.config/fish/functions/fisher.fish
